e59093aa0c2e2c863e715920d6eb25023faafc34
Kaleb Dykema <dykemakaleb@gmail.com>
2023-02-20 01:59:21 -0600

    Partially set up Vue router

--- Vue/index.js
+++ Vue/index.js
@@ -1,5 +1,21 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import './scss/main.scss'
-import App from './App.vue'
+import Home from './views/Home.vue'
+import About from './views/About.vue'
+import Contact from './views/Contact.vue'
 
-createApp(App).mount('#root')
+const routes = [
+  { path: '/', component: Home },
+  { path: '/about', component: About },
+  { path: '/contact', component: Contact },
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+})
+
+const app = createApp(Home)
+app.mount('#root')
+app.use(router)
