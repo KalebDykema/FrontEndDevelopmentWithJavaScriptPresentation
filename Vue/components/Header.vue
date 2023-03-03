46d4a34755aa2e9be34f5cad413c3fdd0c90fb6b
Kaleb Dykema <dykemakaleb@gmail.com>
2023-03-02 22:43:22 -0600

    Refactored Header component styling

--- Vue/components/Header.vue
+++ Vue/components/Header.vue
@@ -1,74 +1,90 @@
 <template>
-  <header>
-    <h2>Vue Site</h2>
-    <nav>
+  <header :class="$style.headerStyles">
+    <h2 :class="$style.siteTitle">
+      <router-link :to="{ name: 'home' }">Vue Site</router-link>
+    </h2>
+    <nav :class="links">
       <router-link :to="{ name: 'home' }">Home</router-link>
       <router-link :to="{ name: 'about' }">About</router-link>
       <router-link :to="{ name: 'contact' }">Contact</router-link>
     </nav>
+    <div :class="$style.colorBars">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
   </header>
 </template>
 
-<style lang="scss">
-  header {
-    width: 100%;
-    outline: 0.25rem solid $primaryYellow;
-    display: flex;
-    align-items: center;
-    gap: 2rem;
-    padding: 3rem 2rem;
-    position: relative;
-    overflow: hidden;
-  }
+<style lang="scss" type="module">
+@import '../scss/variables';
 
-  header nav {
+.headerStyles {
+  width: 100%;
+  outline: 0.25rem solid $primaryYellow;
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+  padding: 3rem 2rem;
+  position: relative;
+  overflow: hidden;
+
+  .links {
     display: flex;
     gap: 0.5rem;
+
+    a {
+      transition: color 0.25s ease-in-out, padding 0.3s ease-in-out;
+
+      &:hover:not(.disabled) {
+        color: $primaryOrange;
+        padding: 0rem 0.25rem;
+      }
+    }
   }
 
-  header h2 a {
+  siteTitle:global(a) {
     transition: transform 0.25s ease-in-out, padding 0.3s ease-in-out;
     color: $primaryYellow;
-  }
-  header h2 a:hover {
-    padding-left: 1rem;
-    text-decoration: none;
+
+    &:hover {
+      padding-left: 1rem;
+      text-decoration: none;
+    }
   }
 
-  header nav a {
-    transition: color 0.25s ease-in-out, padding 0.3s ease-in-out;
-  }
-  header nav a:hover:not(.disabled) {
-    color: $primaryOrange;
-    padding: 0rem 0.25rem;
-  }
-
-  #colorBars {
+  .colorBars {
     display: flex;
     position: absolute;
     top: 0;
     right: 0;
-  }
-  .colorBar {
-    /* height: 4.08rem; */
-    height: 100vh;
-    width: 4rem;
-    transition: width 0.25s ease-out;
-  }
-  .colorBar:hover {
-    width: 6rem;
-  }
-  .colorBar:first-child {
-    background-color: $primaryYellow;
-  }
-  .colorBar:nth-child(2) {
-    background-color: $primaryOrange;
-  }
-  .colorBar:nth-child(3) {
-    background-color: $primaryRed;
-  }
-  .colorBar:nth-child(4) {
-    background-color: $primaryBlue;
-  }
 
+    :global div {
+      height: 100vh;
+      width: 4rem;
+      transition: width 0.25s ease-out;
+
+      &:hover {
+        width: 6rem;
+      }
+
+      &:first-child {
+        background-color: $primaryYellow;
+      }
+
+      &:nth-child(2) {
+        background-color: $primaryOrange;
+      }
+
+      &:nth-child(3) {
+        background-color: $primaryRed;
+      }
+
+      &:nth-child(4) {
+        background-color: $primaryBlue;
+      }
+    }
+  }
+}
 </style>
