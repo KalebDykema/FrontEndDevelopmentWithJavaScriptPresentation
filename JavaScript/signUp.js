e9c160c4e32ab3e6a70a519bb7ed23aca56310e3
Kaleb Dykema <dykemakaleb@gmail.com>
2023-03-07 17:38:42 -0600

    Set up form styling with errors and partial JS solution

--- JavaScript/signUp.js
+++ JavaScript/signUp.js
@@ -1,1 +1,18 @@
-console.log('hello world')
+const form = document.getElementById('newsletterForm')
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const data = new FormData(form)
+  const name = data.get('name')
+  const email = data.get('email')
+
+  if (!name || !email) {
+    displayError()
+    return
+  }
+})
+
+const displayError = () => {
+  console.log('show error')
+}
