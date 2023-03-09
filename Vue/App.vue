e50d70b5b93ca390b34b4342746b990a6b8bf7e7
Kaleb Dykema <dykemakaleb@gmail.com>
2023-03-08 21:22:17 -0600

    Added in footer

--- Vue/App.vue
+++ Vue/App.vue
@@ -12,4 +12,21 @@ export default {
 <template>
   <Header />
   <router-view />
+  <footer :class="$style.footer">Copyright&#169; 2023 Kaleb Dykema</footer>
 </template>
+
+<style lang="scss" module>
+@import './scss/variables';
+
+.footer {
+  width: 100%;
+  background-color: $primaryOrange;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 4rem;
+  position: fixed;
+  bottom: 0;
+  font-weight: bold;
+}
+</style>
