3a3933e026c8db9c9321062dd1438491db1c61f1
Kaleb Dykema <dykemakaleb@gmail.com>
2023-03-08 21:25:18 -0600

    Set up Home view

--- Vue/views/Home.vue
+++ Vue/views/Home.vue
@@ -1,3 +1,46 @@
+<script>
+export default {
+  name: 'Home',
+}
+</script>
+
 <template>
-  <h1>Home</h1>
+  <div :class="$style.home" class="container">
+    <main>
+      <section>
+        <h3>This is my site!</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo
+          voluptatem voluptas possimus. Eum laudantium, magnam voluptas veniam
+          inventore aperiam. Neque excepturi iure odio praesentium nulla
+          incidunt modi quam repellendus doloribus.
+        </p>
+      </section>
+
+      <section>
+        <h3>Section Explaining Something</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+          cumque at ut harum vitae, alias sapiente aut reiciendis iure impedit
+          repellendus placeat dolore veniam ea illum deserunt dignissimos ad
+          totam.
+        </p>
+      </section>
+    </main>
+
+    <h1>Newsletter</h1>
+  </div>
 </template>
+
+<style lang="scss" module>
+.home {
+  display: flex;
+  gap: 1rem;
+  flex-wrap: wrap;
+
+  :global(main) {
+    flex: 2;
+    min-width: 30rem;
+  }
+}
+</style>
