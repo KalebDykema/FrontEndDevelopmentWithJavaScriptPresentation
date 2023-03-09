39e58059bc0916c58e45a87c41bd39315e33abb3
Kaleb Dykema <dykemakaleb@gmail.com>
2023-03-08 21:01:43 -0600

    Refactored routes and router into separate file

--- Vue/index.js
+++ Vue/index.js
@@ -1,40 +1,8 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
-import './scss/main.scss'
+import createRouter from './routes/router'
 import App from './App.vue'
-import Home from './views/Home.vue'
-import About from './views/About.vue'
-import Contact from './views/Contact.vue'
-import NotFound from './views/NotFound.vue'
-
-const routes = [
-  {
-    name: 'home',
-    path: '/',
-    component: Home,
-  },
-  {
-    name: 'about',
-    path: '/about',
-    component: About,
-  },
-  {
-    name: 'contact',
-    path: '/contact',
-    component: Contact,
-  },
-  {
-    name: 'not-found',
-    path: '/:pathMatch(.*)*',
-    component: NotFound,
-  },
-]
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes,
-})
+import './scss/main.scss'
 
 const app = createApp(App)
-app.use(router)
+app.use(createRouter)
 app.mount('#root')
